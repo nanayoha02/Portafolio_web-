0cc80264d4b28b2c0b7847b8194c54e957b39cf0
nanayoha02 <franceschiyohana1@gmail.com>
2026-04-08 05:37:37 +0000

    08

--- js/config.js
+++ js/config.js
@@ -1,6 +1,10 @@
-const SUPABASE_URL = "https://hydpocjkzsrsfbwgdodi.supabase.co";
-const SUPABASE_KEY = "sb_publishable_HnGLMO92-LnYnnPhFIYkEQ__NGTp_SJ";
+if (window._supabase) {
+    console.log("Supabase ya inicializado");
+} else {
+    window._supabase = supabase.createClient(
+        "https://hydpocjkzsrsfbwgdodi.supabase.co",
+        "sb_publishable_HnGLMO92-LnYnnPhFIYkEQ__NGTp_SJ"
+    );
 
-window._supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-window.dispatchEvent(new Event("supabaseReady"));
+    window.dispatchEvent(new Event("supabaseReady"));
+}
